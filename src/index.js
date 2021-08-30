626212a50a311df3850f93877877909647d6ae7f
Eder Lima <devilsouls666@gmail.com>
2021-08-30 18:15:30 -0300

    Updated files and tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs/promises')
 require('./types')
 
 class Todos {
@@ -6,21 +7,35 @@ class Todos {
    */
   #todos
 
+  /**
+   * Instanciates a new "Todos" object
+   */
   constructor() {
     this.#todos = []
   }
 
+  /**
+   * Returns a list of all the todos
+   *
+   * @returns {Todo[]}
+   */
   list() {
     return [...this.#todos]
   }
 
+  /**
+   * Adds a todo
+   *
+   * @param {string} title Title of the todo to be added
+   */
   add(title) {
-    this.#todos.push({
-      title,
-      completed: false,
-    })
+    this.#todos.push(this.#todoFactory(title))
   }
 
+  /**
+   * Completes a todo
+   * @param {string} title Title to filter the todo
+   */
   complete(title) {
     if (this.#todos.length === 0)
       throw new Error(`You have no TODOs stored. Please add one first.`)
@@ -30,6 +45,33 @@ class Todos {
     if (!todo) throw new Error(`Todo with title "${title}" was not found.`)
 
     todo.completed = true
+  }
+
+  /**
+   * Saves the todos to a file
+   */
+  async saveToFile() {
+    const csvHeader = 'Title,Completed\n'
+
+    const csvFileContent = this.#todos.reduce(
+      (content, todo) => (content += `${todo.title},${todo.completed}\n`),
+      csvHeader
+    )
+
+    await fs.writeFile('todos.csv', csvFileContent)
+  }
+
+  /**
+   * Generates a todo object
+   *
+   * @param {string} title Title of the todo
+   * @returns {Todo} Todo object
+   */
+  #todoFactory(title) {
+    return {
+      title,
+      completed: false,
+    }
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
 const Todos = require('../src/index')
 const assert = require('assert/strict')
+const fs = require('fs/promises')
+const fsSync = require('fs')
 
 describe('Integration test', () => {
   let todos = new Todos()
@@ -8,44 +10,97 @@ describe('Integration test', () => {
     todos = new Todos()
   })
 
-  it('should return 0 as the todos length', () => {
-    assert.strictEqual(todos.list().length, 0)
-  })
-
-  it('should add a todo to the todo list', () => {
+  it('should add and complete a todo', () => {
     todos.add('Hello, world!')
+    const todoList = todos.list()
 
-    const EXPECTED_RESULT = [
-      {
-        title: 'Hello, world!',
-        completed: false,
-      },
-    ]
+    const EXPECTED_RESULT = [{ title: 'Hello, world!', completed: false }]
 
-    assert.deepStrictEqual(todos.list(), EXPECTED_RESULT)
+    assert.deepStrictEqual(EXPECTED_RESULT, todoList)
+    assert.strictEqual(todoList.length, 1)
+  })
+})
+
+describe('complete()', () => {
+  let todos = new Todos()
+
+  beforeEach(() => {
+    todos = new Todos()
   })
 
   it('should complete a todo', () => {
     todos.add('Hello, world!')
-
-    const EXPECTED_RESULT = [
-      {
-        title: 'Hello, world!',
-        completed: true,
-      },
-    ]
-
     todos.complete('Hello, world!')
+
+    const EXPECTED_RESULT = [{ title: 'Hello, world!', completed: true }]
 
     assert.deepStrictEqual(todos.list(), EXPECTED_RESULT)
   })
 
-  it('should throw an error when trying to complete a todo on an empty todo list', () => {
+  it('should throw an error if there are no todos', () => {
     assert.throws(() => todos.complete(null), Error)
   })
 
   it('should throw an error when trying to complete an unexisting todo', () => {
-    todos.add('Hello, world!')
+    todos.add('hmmmm')
     assert.throws(() => todos.complete(null), Error)
   })
+})
+
+describe('add()', () => {
+  let todos = new Todos()
+
+  beforeEach(() => (todos = new Todos()))
+
+  it('should add a todo to the todo list', () => {
+    todos.add('null')
+    const EXPECTED_RESULT = [{ title: 'null', completed: false }]
+
+    assert(todos.list(), EXPECTED_RESULT)
+  })
+})
+
+describe('list()', () => {
+  let todos = new Todos()
+
+  beforeEach(() => (todos = new Todos()))
+
+  it('should return a cloned version of the todos stored in the instanciated "Todos" object', () => {
+    todos.add('null')
+    const todoList = todos.list()
+    const EXPECTED_RESULT = [{ title: 'null', completed: false }]
+    assert.deepStrictEqual(todoList, EXPECTED_RESULT)
+  })
+})
+
+describe('saveToFile()', () => {
+  let todos = new Todos()
+
+  const cleanupFunction = async () => {
+    if (fsSync.existsSync('todos.csv')) await fs.rm('todos.csv')
+  }
+
+  beforeEach(() => (todos = new Todos()))
+
+  it('should save the file', async () => {
+    todos.add('Hello, world!')
+    await todos.saveToFile()
+    assert.strictEqual(fsSync.existsSync('todos.csv'), true)
+  })
+
+  it('should save the file and have a single todo', async () => {
+    todos.add('Saving to a CSV file')
+
+    await todos.saveToFile()
+
+    const EXPECTED_RESULT = 'Title,Completed\nSaving to a CSV file,false\n'
+    const fileBuffer = await fs.readFile('todos.csv')
+    const fileContent = fileBuffer.toString()
+
+    assert.strictEqual(fileContent, EXPECTED_RESULT)
+  })
+
+  // Cleanup function to remove the file
+  afterEach(cleanupFunction)
+  after(cleanupFunction)
 })
